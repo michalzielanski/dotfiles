8f77e05bab385a7be70c7fa691e24fd071fcf953
Michał Zielański <michalzielanski@gmail.com>
2026-02-26 12:54:46 +0100

    zsh: dl alias (download with curl)

--- dot_config/zsh/conf.d/30-aliases.zsh
+++ dot_config/zsh/conf.d/30-aliases.zsh
@@ -4,6 +4,8 @@ alias grep="${aliases[grep]:-grep} --exclude-dir={.git,.svn,.vscode,.idea}"
 
 alias tarls='tar -tvf'
 
+alias dl='curl --continue-at - --location --progress-bar --remote-name --remote-time'
+
 alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
 
 alias print-fpath='for fp in $fpath; do echo $fp; done; unset fp'
@@ -11,3 +13,4 @@ alias print-opts='setopt | sed "s/^/ON  /"; unsetopt | sed "s/^/OFF /"'
 alias print-default-opts='emulate -lLR zsh'
 alias print-path='echo $PATH | tr ":" "\n"'
 alias print-functions='print -l ${(k)functions[(I)[^_]*]} | sort'
+
